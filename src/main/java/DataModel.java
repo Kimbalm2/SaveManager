9cef37e6197022ffc212f737acb08ea5c97228fd
Mitch Kimball <mitchkimball16@gmail.com>
2021-03-06 16:54:43 -0800

    Added local JSON storage for tracked games and the file paths entered.

--- src/main/java/DataModel.java
+++ src/main/java/DataModel.java
@@ -1,22 +1,27 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class DataModel {
 
-    private static DataModel instance = new DataModel();
-    private MongoDatabase mongoDatabase;
+    private static final DataModel instance = new DataModel();
+    private final MongoDatabase mongoDatabase;
+    private ArrayList<gameEntity> gameList;
 
     private SaveManagerUI saveManagerUI;
     private DataModel() {
         mongoDatabase = getDbConnection();
-        //store all Game titles and their filepaths to saved games
-        //store the db connection?
+        loadData();
     }
 
     public static DataModel getInstance(){
@@ -41,7 +46,54 @@ public class DataModel {
         }
         return scan.nextLine();
     }
+
+    public void saveData(){
+        Gson gson = new Gson();
+        String gameJson = gson.toJson(gameList);
+        //write to file
+        String currentDir = System.getProperty("user.dir");
+        currentDir = currentDir + "\\src\\main\\resources\\gamePaths.json";
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(currentDir);
+            byte[] bytes = gameJson.getBytes();
+            fileOutputStream.write(bytes);
+            fileOutputStream.close();
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    private void loadData(){
+        //TODO remove when finished with add new game logic.
+        //String founderJson = "[{'gameName': 'Christian','filePath': 1}, {'gameName': 'Marcus', 'filePath': 3}, {'gameName': 'Norman', 'filePath': 2}]";
+        String currentDir = System.getProperty("user.dir");
+        currentDir = currentDir + "\\src\\main\\resources\\gamePaths.json";
+        File jsonFile = new File(currentDir);
+        try{
+            Scanner scanner = new Scanner(jsonFile);
+            String jsonString =scanner.nextLine();
+            Gson gson = new Gson();
+            Type founderListType = new TypeToken<ArrayList<gameEntity>>(){}.getType();
+            gameList = gson.fromJson(jsonString, founderListType);
+        }
+        catch(IOException ex){
+            gameList = new ArrayList<gameEntity>();
+        }
+
+
+    }
+
     public MongoDatabase getMongoDatabase() {
         return mongoDatabase;
     }
+
+    public ArrayList<gameEntity> getGameList() {
+        return gameList;
+    }
+
+    public void addToGameList(gameEntity gameEntity) {
+        this.gameList.add(gameEntity);
+    }
+
 }
