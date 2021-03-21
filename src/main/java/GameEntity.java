274dc95a4bb346b4bb51c9591f41fc2bed163cbc
Mitch <mitchkimball16@gmail.com>
2021-03-21 14:02:31 -0700

    Added some more data to GameEntity, this should make it easier in the future to find files in the DB. Added a toDocument method to convert the entity data into a document for GridFS meta data file options.

--- src/main/java/GameEntity.java
+++ src/main/java/GameEntity.java
@@ -1,11 +1,16 @@
+import org.bson.Document;
 
 public class GameEntity {
     private final String gameName;
     private final String filePath;
+    private final String fileName;
+    private final boolean isFolder;
 
-    public GameEntity(String gameName, String filePath){
+    public GameEntity(String gameName, String filePath, String fileName, boolean isFolder){
         this.gameName = gameName;
         this.filePath = filePath;
+        this.fileName = fileName;
+        this.isFolder = isFolder;
     }
 
     public String getGameName() {
@@ -14,5 +19,20 @@ public class GameEntity {
 
     public String getFilePath() {
         return filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public boolean isFolder() {
+        return isFolder;
+    }
+
+    public Document toDocument(){
+        return new Document()
+                .append("Game Title",gameName)
+                .append("File Name",fileName)
+                .append("isFolder",isFolder);
     }
 }
